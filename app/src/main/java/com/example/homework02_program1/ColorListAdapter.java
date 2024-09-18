3b09762495aa58379ac115d06e43ee5db7cbf482
masonmclaughlin530 <masonmclaughlin530@gmail.com>
2024-09-18 14:13:56 -0400

    09-18-24
    Finished

--- app/src/main/java/com/example/homework02_program1/ColorListAdapter.java
+++ app/src/main/java/com/example/homework02_program1/ColorListAdapter.java
@@ -1,33 +1,87 @@
 package com.example.homework02_program1;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ColorListAdapter extends BaseAdapter
 {
+    Context context;
+    ArrayList<ColorInfo> listOfColors;
+
+
+    public ColorListAdapter(Context c, ArrayList<ColorInfo> lc)
+    {
+        context = c;
+        listOfColors = lc;
+    }
 
     @Override
     public int getCount()
     {
-        return 0;
+        return listOfColors.size();
     }
 
     @Override
     public Object getItem(int i)
     {
-        return null;
+        return listOfColors.get(i);
     }
 
     @Override
     public long getItemId(int i)
     {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup)
     {
-        return null;
+        if (view == null)
+        {
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(MainActivity.LAYOUT_INFLATER_SERVICE);
+            view = mInflater.inflate(R.layout.custom_cell, null);
+            //Log.d("GETVIEW IF STATEMENT", "In IF statement");
+        }
+
+        TextView red = view.findViewById(R.id.tv_v_cc_red);
+        TextView green = view.findViewById(R.id.tv_v_cc_green);
+        TextView blue = view.findViewById(R.id.tv_v_cc_blue);
+        TextView hex = view.findViewById(R.id.tv_v_cc_hex);
+
+        ColorInfo color = listOfColors.get(i);
+
+        red.setText("Red: " + color.getRed());
+        blue.setText("Blue: " + color.getBlue());
+        green.setText("Green: " + color.getGreen());
+        hex.setText("Hex: " + color.getHex());
+
+        if(color.getRed() <= 100 && color.getGreen() <= 100 && color.getBlue() <= 100)
+        {
+            red.setTextColor(Color.parseColor("#FFFFFF"));
+            blue.setTextColor(Color.parseColor("#FFFFFF"));
+            green.setTextColor(Color.parseColor("#FFFFFF"));
+            hex.setTextColor(Color.parseColor("#FFFFFF"));
+        }
+        else
+        {
+            red.setTextColor(Color.parseColor("#000000"));
+            blue.setTextColor(Color.parseColor("#000000"));
+            green.setTextColor(Color.parseColor("#000000"));
+            hex.setTextColor(Color.parseColor("#000000"));
+        }
+
+        view.setBackgroundColor(Color.parseColor(color.getHex()));
+        //Log.d("Get View", "In get view past the if statement");
+
+        return view;
     }
 }
--- app/src/main/java/com/example/homework02_program1/MainActivity.java
+++ app/src/main/java/com/example/homework02_program1/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -51,6 +52,9 @@ public class MainActivity extends AppCompatActivity
     ArrayList<ColorInfo> listOfColors;
 
 
+    ColorListAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,7 +91,11 @@ public class MainActivity extends AppCompatActivity
         greenSeekBarEventListener();
         blueSeekBarEventListener();
         saveColorButtonEvent();
+        lvOnClickListener();
 
+        listOfColors = new ArrayList<ColorInfo>();
+
+        fillListView();
 
 
     }
@@ -223,8 +231,8 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view)
             {
                 addColorToList();
-                Log.d("Color Added", "Color was added to the list");
 
+                //listColorData();
             }
         });
     }
@@ -232,6 +240,9 @@ public class MainActivity extends AppCompatActivity
     private void addColorToList()
     {
         ColorInfo colorToAdd = new ColorInfo(red,green,blue,hex);
+
+        listOfColors.add(colorToAdd);
+        Log.d("Color Added", "Color was added to the list");
 
         sb_j_red.setProgress(255);
         sb_j_green.setProgress(255);
@@ -241,5 +252,62 @@ public class MainActivity extends AppCompatActivity
         tv_j_green.setText("Green: 255");
         tv_j_blue.setText("Blue: 255");
         tv_j_hexRep.setText("Hex representation: " + hex);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    private void fillListView ()
+    {
+        adapter = new ColorListAdapter(this, listOfColors);
+
+
+        lv_j_listOfColors.setAdapter(adapter);
+
+
+    }
+
+    private void listColorData()
+    {
+        for (int i = 0; i < listOfColors.size();i++)
+        {
+            Log.d("red: ", listOfColors.get(i).getRed() + "");
+        }
+    }
+
+    private void lvOnClickListener()
+    {
+        lv_j_listOfColors.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                Log.d("onItemClick: ", "You Clicked Item: " + l + " at position " + position);
+
+                background.setBackgroundColor(Color.parseColor(listOfColors.get(position).getHex()));
+
+                tv_j_red.setText("Red: " + String.valueOf(listOfColors.get(position).getRed()));
+                red = listOfColors.get(position).getRed();
+                sb_j_red.setProgress(red);
+                tv_j_green.setText("Green: " + String.valueOf(listOfColors.get(position).getGreen()));
+                green = listOfColors.get(position).getGreen();
+                sb_j_green.setProgress(green);
+                tv_j_blue.setText("Blue: " + String.valueOf(listOfColors.get(position).getBlue()));
+                blue = listOfColors.get(position).getBlue();
+                sb_j_blue.setProgress(blue);
+                if(red <= 100 && green <= 100 && blue <= 100)
+                {
+                    tv_j_red.setTextColor(Color.parseColor("#FFFFFF"));
+                    tv_j_blue.setTextColor(Color.parseColor("#FFFFFF"));
+                    tv_j_green.setTextColor(Color.parseColor("#FFFFFF"));
+                    tv_j_hexRep.setTextColor(Color.parseColor("#FFFFFF"));
+                }
+                else
+                {
+                    tv_j_red.setTextColor(Color.parseColor("#000000"));
+                    tv_j_blue.setTextColor(Color.parseColor("#000000"));
+                    tv_j_green.setTextColor(Color.parseColor("#000000"));
+                    tv_j_hexRep.setTextColor(Color.parseColor("#000000"));
+                }
+            }
+        });
     }
 }
